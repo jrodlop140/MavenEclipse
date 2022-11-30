9d8fcda0d6ced2576e4343730a8ab049ab31ee2e
jrodlop140 <116264748+jrodlop140@users.noreply.github.com>
2022-11-30 21:17:29 +0100

    Add files via upload

---
+++ fibonacciMain/src/main/java/es/iescarrillo/daw/endes/fibonacciMain/Fibonacci2.java
@@ -0,0 +1,11 @@
+package es.iescarrillo.daw.endes.fibonacciMain;
+import es.iescarrillo.daw.endes.fibonacci.*;
+
+public class Fibonacci2 
+{
+    public static void main( String[] args ){
+    	Fibonacci.main(null);
+      
+    	
+    }
+}
